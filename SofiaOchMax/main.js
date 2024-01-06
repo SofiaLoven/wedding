80f76936d2420aff7339bee7177f3ae752de38ba
SofiaLoven <113837939+SofiaLoven@users.noreply.github.com>
2024-01-06 11:35:49 +0100

    Added files via upload
    First commit

---
+++ SofiaOchMax/main.js
@@ -0,0 +1,6 @@
+const tablePlacingBtn = document.querySelector("#tablePlacing");
+
+tablePlacingBtn.addEventListener("click", () => {
+  window.open("https://youtu.be/DLzxrzFCyOs?si=itvRj01tuhOm3tD5", "_blank");
+  tablePlacingBtn.innerText = "Lurad";
+});
